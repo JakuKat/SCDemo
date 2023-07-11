3036c7aac932ad0dc01b39f0f8df5abdf1f5d476
JakuKat <jakkatzer@gmail.com>
2023-07-11 13:56:51 +0200

    Feat: added react-player

--- src/pages/Playground.tsx
+++ src/pages/Playground.tsx
@@ -1,11 +1,16 @@
+import ReactPlayer from "react-player";
+
 export const PlaygroundPage = () => {
   return (
     <>
       <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-        <div className="text-center">
+        <div className="text-center mb-8">
           <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
             Playground space
           </h1>
+        </div>
+        <div>
+          <ReactPlayer url="https://youtu.be/dhBtlVfFdBw" />
         </div>
       </main>
     </>
